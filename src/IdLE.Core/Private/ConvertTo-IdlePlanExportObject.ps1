7682236e3b56ecaad90a8bcd257737701264f99e
Matthias Fleschuetz <13959569+blindzero@users.noreply.github.com>
2026-01-04 19:36:22 +0100

    Revert "core: redact plan export inputs and expected state"
    This reverts commit 22cb9a3800f4c0ea69cf357752e773227f281126.

--- src/IdLE.Core/Private/ConvertTo-IdlePlanExportObject.ps1
+++ src/IdLE.Core/Private/ConvertTo-IdlePlanExportObject.ps1
@@ -129,19 +129,11 @@ function ConvertTo-IdlePlanExportObject {
         $requestInput = $null
     }
 
-    # Redact request.input at the export boundary (do not mutate the original request object).
-    $redactedRequestInput = if ($null -ne $requestInput) {
-        Copy-IdleRedactedObject -Value $requestInput
-    }
-    else {
-        $null
-    }
-
     $requestMap = New-OrderedMap
     $requestMap.type          = $requestType
     $requestMap.correlationId = $correlationId
     $requestMap.actor         = $actor
-    $requestMap.input         = $redactedRequestInput
+    $requestMap.input         = $requestInput
 
     # ---- Plan block ----------------------------------------------------------
     $planId = ConvertTo-NullIfEmptyString -Value (
@@ -217,29 +209,14 @@ function ConvertTo-IdlePlanExportObject {
         $inputs = Get-FirstPropertyValue -Object $step -Names @('Inputs', 'Input', 'Parameters', 'Arguments', 'With')
         $expectedState = Get-FirstPropertyValue -Object $step -Names @('ExpectedState', 'DesiredState', 'TargetState')
 
-        # Redact step inputs / expectedState at the export boundary (do not mutate original step objects).
-        $redactedInputs = if ($null -ne $inputs) {
-            Copy-IdleRedactedObject -Value $inputs
-        }
-        else {
-            $null
-        }
-
-        $redactedExpectedState = if ($null -ne $expectedState) {
-            Copy-IdleRedactedObject -Value $expectedState
-        }
-        else {
-            $null
-        }
-
         $stepMap = New-OrderedMap
         $stepMap.id = $stepId
         $stepMap.name = $stepName
         $stepMap.stepType = $stepType
         $stepMap.provider = $provider
         $stepMap.condition = $conditionMap
-        $stepMap.inputs = $redactedInputs
-        $stepMap.expectedState = $redactedExpectedState
+        $stepMap.inputs = $inputs
+        $stepMap.expectedState = $expectedState
 
         $stepList += $stepMap
     }
