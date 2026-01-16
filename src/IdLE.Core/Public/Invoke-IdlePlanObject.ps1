f4b4ad3238e2de51ad54ec6dc040cb1df02a0eb9
Matthias Fleschuetz <13959569+blindzero@users.noreply.github.com>
2026-01-17 00:26:14 +0100

    core: extract Assert-IdleNoScriptBlockInAuthSessionOptions to private function to avoid duplication

--- src/IdLE.Core/Public/Invoke-IdlePlanObject.ps1
+++ src/IdLE.Core/Public/Invoke-IdlePlanObject.ps1
@@ -144,52 +144,6 @@ function Invoke-IdlePlanObject {
         return $null
     }
 
-    function Assert-IdleNoScriptBlockInAuthSessionOptions {
-        [CmdletBinding()]
-        param(
-            [Parameter(Mandatory)]
-            [AllowNull()]
-            [object] $InputObject,
-
-            [Parameter(Mandatory)]
-            [ValidateNotNullOrEmpty()]
-            [string] $Path
-        )
-
-        if ($null -eq $InputObject) { return }
-
-        if ($InputObject -is [scriptblock]) {
-            throw [System.ArgumentException]::new(
-                "ScriptBlocks are not allowed in auth session options. Found at: $Path",
-                $Path
-            )
-        }
-
-        if ($InputObject -is [System.Collections.IDictionary]) {
-            foreach ($key in $InputObject.Keys) {
-                Assert-IdleNoScriptBlockInAuthSessionOptions -InputObject $InputObject[$key] -Path "$Path.$key"
-            }
-            return
-        }
-
-        if (($InputObject -is [System.Collections.IEnumerable]) -and ($InputObject -isnot [string])) {
-            $i = 0
-            foreach ($item in $InputObject) {
-                Assert-IdleNoScriptBlockInAuthSessionOptions -InputObject $item -Path "$Path[$i]"
-                $i++
-            }
-            return
-        }
-
-        if ($InputObject -is [pscustomobject]) {
-            foreach ($p in $InputObject.PSObject.Properties) {
-                if ($p.MemberType -eq 'NoteProperty') {
-                    Assert-IdleNoScriptBlockInAuthSessionOptions -InputObject $p.Value -Path "$Path.$($p.Name)"
-                }
-            }
-        }
-    }
-
     $planPropNames = @($Plan.PSObject.Properties.Name)
 
     $request = if ($planPropNames -contains 'Request') { $Plan.Request } else { $null }
@@ -273,52 +227,6 @@ function Invoke-IdlePlanObject {
             [AllowNull()]
             [hashtable] $Options
         )
-
-        function Assert-IdleNoScriptBlockInAuthSessionOptions {
-            [CmdletBinding()]
-            param(
-                [Parameter(Mandatory)]
-                [AllowNull()]
-                [object] $InputObject,
-
-                [Parameter(Mandatory)]
-                [ValidateNotNullOrEmpty()]
-                [string] $Path
-            )
-
-            if ($null -eq $InputObject) { return }
-
-            if ($InputObject -is [scriptblock]) {
-                throw [System.ArgumentException]::new(
-                    "ScriptBlocks are not allowed in auth session options. Found at: $Path",
-                    $Path
-                )
-            }
-
-            if ($InputObject -is [System.Collections.IDictionary]) {
-                foreach ($key in $InputObject.Keys) {
-                    Assert-IdleNoScriptBlockInAuthSessionOptions -InputObject $InputObject[$key] -Path "$Path.$key"
-                }
-                return
-            }
-
-            if (($InputObject -is [System.Collections.IEnumerable]) -and ($InputObject -isnot [string])) {
-                $i = 0
-                foreach ($item in $InputObject) {
-                    Assert-IdleNoScriptBlockInAuthSessionOptions -InputObject $item -Path "$Path[$i]"
-                    $i++
-                }
-                return
-            }
-
-            if ($InputObject -is [pscustomobject]) {
-                foreach ($p in $InputObject.PSObject.Properties) {
-                    if ($p.MemberType -eq 'NoteProperty') {
-                        Assert-IdleNoScriptBlockInAuthSessionOptions -InputObject $p.Value -Path "$Path.$($p.Name)"
-                    }
-                }
-            }
-        }
 
         $providers = $this.Providers
         $broker = $null
